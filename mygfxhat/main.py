1338d6d0f89967820137a92366670d51c9ecb5f3
Johan <johan.chapelain@gmail.com>
2020-09-05 16:12:37 +0200

    fix

--- mygfxhat/main.py
+++ mygfxhat/main.py
@@ -27,8 +27,8 @@ class DisplayObject:
                 print('you are in the function draw')
                 print(text)
                 w, h = self.font.getsize(text)
-                x = padding
-                y = (self.screen_height - h) // 2
+                x = 2
+                y = (self.screen_height - h) // 2 + padding
                 print("screen height", self.screen_height, "screen width", self.screen_width)
                 print(x, y , text)
                 self.draw.text((x, y), text, 1, self.font)
