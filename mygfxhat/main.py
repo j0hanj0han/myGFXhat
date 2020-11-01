bfd56a1f3a342a8b1b71aa7f4ac73e07ece7aefb
Johan <johan.chapelain@gmail.com>
2020-11-01 19:02:57 +0100

    test

--- mygfxhat/main.py
+++ mygfxhat/main.py
@@ -24,18 +24,20 @@ playlist = ["http://cdn.nrjaudio.fm/audio1/fr/40102/aac_576.mp3","http://direct.
     
 def launch_radio(channel, event):
     print("coucou dans le bouton!")
-    if player: 
+    if player in locals(): 
         if player.is_playing() == 1:
                 print("Lecture en cours...")
                 print("On arrete la lecture")
                 player.stop()
         else:
-            try:
-                print("On lance la lecture...")
-                player = vlc.MediaPlayer(playlist[0])
-                radio = player.play()
-            except Exception as e:
-                print(e)
+            player.play()
+    else:
+        try:
+            print("On lance la lecture...")
+            player = vlc.MediaPlayer(playlist[0])
+            radio = player.play()
+        except Exception as e:
+            print(e)
 
 
 
