5e48c19b7696c5ac38fb91ec0817a8677a2d7e6a
Johan <johan.chapelain@gmail.com>
2020-11-01 17:22:42 +0100

    import handler

--- mygfxhat/main.py
+++ mygfxhat/main.py
@@ -1,12 +1,23 @@
 import display
 import time
 from display import Screen
+import signal
+from gfxhat import touch
+
+def handler(channel, event):
+    print("Got {} on channel {}".format(event, channel))
+
+
+
 
 if __name__ == "__main__":
     object_list = ["Hello Johan !", "Hello Nina !", "Go to Nice!"]
+    for x in range(6):
+        touch.on(x, handler)
+
     screen = Screen(object_list)
     screen.start()
     screen.draw_object(object_list)
-    time.sleep(5)
+    signal.pause()
     screen.stop()
     pass
