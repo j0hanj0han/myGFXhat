c01bea86acc1208589ee3b3a4caefa5fe57eb0db
Johan <johan.chapelain@gmail.com>
2020-11-02 09:16:04 +0100

    refacto

---
+++ mygfxhat/handler/handler.py
@@ -0,0 +1,19 @@
+from gfxhat import touch
+
+
+class Handler:
+    ''' When instantiate this, put functions to buttons of GFXHAT '''
+    def __init__(self, buttons):
+        initialize_button()
+
+    def initialize_button():
+        for x in range05
+
+         #activate handler
+    for x in range(5):
+        touch.on(x, handler)
+
+    touch.on(4, display_time)
+
+    touch.on(5, launch_radio)   
+
---
+++ mygfxhat/handler/handler_function.py
@@ -0,0 +1,32 @@
+import time
+
+# handler for touch
+def handler(channel, event):
+    print("Got {} on channel {}".format(event, channel))
+
+def display_time(channel, event):
+    now = datetime.datetime.now()
+    print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+# radio section
+playlist = ["http://cdn.nrjaudio.fm/audio1/fr/40102/aac_576.mp3","http://direct.franceinfo.fr/live/franceinfo-midfi.mp3"]
+    
+def launch_radio(channel, event):
+    print("coucou dans le bouton!")
+    if 'player' in locals(): 
+        if player.is_playing() == 1:
+                print("Lecture en cours...")
+                print("On arrete la lecture")
+                player.stop()
+        else:
+            print("On relance la lecture")
+            player.play()
+    else:
+        try:
+            print("On lance la lecture...")
+            player = vlc.MediaPlayer(playlist[0])
+            radio = player.play()
+        except Exception as e:
+            print(e)
+
