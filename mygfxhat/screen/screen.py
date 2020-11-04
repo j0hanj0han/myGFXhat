2f3abacd3519615427333f8d9237aec735da6506
Johan <johan.chapelain@gmail.com>
2020-11-04 08:35:30 +0100

    refacto

--- mygfxhat/screen/screen.py
+++ mygfxhat/screen/screen.py
@@ -18,7 +18,9 @@ class Screen:
         self.image = Image.new("P", (self.screen_width, self.screen_height))
         self.draw = ImageDraw.Draw(self.image)
         self.font = ImageFont.load_default()
-        self.buttons = h.Handler()
+        self.player_stmt = False
+        self.buttons = self.initialize_button()
+
 
     def draw_object(self, object_list):
         padding = 2
@@ -59,3 +61,35 @@ class Screen:
         backlight.show()
         lcd.clear()
         lcd.show()
+
+    def initialize_button(self):
+        touch.on(0, f.handler)
+        touch.on(1, f.handler)
+        touch.on(2, f.handler)
+        touch.on(3, self.illuminate)
+        touch.on(4, f.display_time)
+        touch.on(5, self.launch_radio)
+
+
+    def launch_radio(self, channel, event): 
+        #import pdb; pdb.set_trace()
+        print("on rentre dans la fonction player_stmt:", self.player_stmt)
+        if self.player_stmt == True: 
+            self.player.stop()
+            self.player_stmt = False
+            touch.set_led(5, 0)
+            print("GOOD : On arrete la lecture, player_stmt", self.player_stmt)
+            return self.player_stmt
+        elif self.player_stmt == False:
+            self.player = vlc.MediaPlayer(self.playlist[0])
+            radio = self.player.play()
+            self.player_stmt = True
+            touch.set_led(5, 1)
+            print("on lance la lecture, player_stmt:", self.player_stmt)
+            return self.player_stmt
+
+    def illuminate(self, channel, event):
+        for x in range(6):
+            touch.set_led(x, 1)
+            time.sleep(0.1)
+            touch.set_led(x, 0)
