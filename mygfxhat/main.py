613f1e5da2c841c06a2fd10eb136d12a290ebd3b
Johan <johan.chapelain@gmail.com>
2020-11-12 20:20:04 +0100

    fix

--- mygfxhat/main.py
+++ mygfxhat/main.py
@@ -11,20 +11,20 @@ from screen import screen
 def main():
     object_list = ["Hello Johan !", "Hello Nina !", "Go to Nice!"]
     
+    while True: 
     # light on screen and display object list
-    try: 
-        myscreen = screen.Screen(object_list)
-        myscreen.start()
-        myscreen.draw_object(object_list)
-        signal.pause()
+        try: 
+            myscreen = screen.Screen(object_list)
+            myscreen.start()
+            myscreen.draw_object(object_list)
 
-    except KeyboardInterrupt: # if exit with ctrl + c shut off the screen
-        print('Interrupted')
-        try:
-            myscreen.stop()
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+        except KeyboardInterrupt: # if exit with ctrl + c shut off the screen
+            print('Interrupted')
+            try:
+                myscreen.stop()
+                sys.exit(0)
+            except SystemExit:
+                os._exit(0)
 
 if __name__ == '__main__':
    main()
