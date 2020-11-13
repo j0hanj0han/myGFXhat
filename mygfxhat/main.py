0cf4d2653c95cdffa17199a54be978189439ebf4
Johan <johan.chapelain@gmail.com>
2020-11-13 11:14:49 +0100

    remove while loop

--- mygfxhat/main.py
+++ mygfxhat/main.py
@@ -5,22 +5,12 @@ from gfxhat import lcd
 #internal import
 from screen import screen
 
-
-
-
-
-
-
 def main():
-    object_list = ["Hello Johan !", "Hello Nina !", "Go to Nice!"]
-    # light on screen and display object list
-    myscreen = screen.Screen(object_list)
-    while True: 
         try: 
             now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             object_list = [now, "Hello Johan !", "Hello Nina !", "Go to Nice!"]
             myscreen.draw_object(object_list)
-
+            signal.pause()
         except KeyboardInterrupt: # if exit with ctrl + c shut off the screen
             print('Interrupted')
             try:
