6b6e3a6952e4e2c1444f3aa072b7b825fe926a9c
Johan <johan.chapelain@gmail.com>
2020-11-13 08:56:23 +0100

    fix height

--- mygfxhat/screen/screen.py
+++ mygfxhat/screen/screen.py
@@ -41,7 +41,7 @@ class Screen:
             # print(text)
             w, h = self.font.getsize(text)
             x = 2
-            y = (self.screen_height - h) // 2 + padding
+            y = (self.screen_height - h) + padding
             # print(
             #     "screen height", self.screen_height, "screen width", self.screen_width
             # )
