e0cfd53fe5a8b85c135ea4fea3485757b798eef7
Johan <johan.chapelain@gmail.com>
2020-11-13 08:35:14 +0100

    fix

--- mygfxhat/main.py
+++ mygfxhat/main.py
@@ -20,9 +20,7 @@ def main():
             now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             object_list = [now, "Hello Johan !", "Hello Nina !", "Go to Nice!"]
             myscreen.draw_object(object_list)
-            time.sleep(1)
-            lcd.clear()
-            lcd.show()
+
         except KeyboardInterrupt: # if exit with ctrl + c shut off the screen
             print('Interrupted')
             try:
